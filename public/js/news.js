02e3d5755dfeff34b1c23d6ad9f4368698c59bb1
Venkata Rapaka <Venkata.Rapaka@liquidityservices.com>
2018-03-27 13:52:18 -0500

    news ui

--- public/js/news.js
+++ public/js/news.js
@@ -1,17 +1,12 @@
-(function () {
-    
-    
-    var skycons = new Skycons({ "color": "green" });
-    // on Android, a nasty hack is needed: {"resizeClear": true}
-    // you can add a canvas by it's ID...
-    
-    skycons.add("icon_current", $('#icon_current').attr("class"))
-    $.each($('.skycon > canvas'), function(index, element){
-        var elementId = `icon_day${index}`;
-        console.log(elementId)
-        skycons.add(elementId, $('#' + elementId).attr("class"))        
-    })
-    
-    // start animation!
-    skycons.play();
+(function () {        
+    $('.news-items').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 30000,
+        arrows: false,
+        infinite: true,
+        rows: 2
+      });
+          
 })()
