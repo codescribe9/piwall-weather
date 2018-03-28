c1a6ccac3121a2faeb5e42b2f2e1938dbc02eddc
Venkata Rapaka <Venkata.Rapaka@liquidityservices.com>
2018-03-28 13:46:11 -0500

    update to quotes ui

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -1,13 +1,25 @@
 (function () {
-    toggleQuotes()
-    setInterval(toggleQuotes, 3000)
+    $('.quotes').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        arrows: false,
+        infinite: true
+      });
 
+    //$('.columns[category="funny"]').toggle()
+    //toggleQuotes()
+    //setInterval(toggleQuotes, 3000)
+    
 })()
 
 var toggle = true;
-function toggleQuotes() {
-    $('.columns[category="inspire"]').toggle(toggle).slideDown({duration: 800, easing: 'swing'})
-    $('.columns[category="funny"]').toggle(toggle).slideUp({duration: 800, easing: 'swing'})
+function toggleQuotes() {        
+   if(toggle == true)
+        $('.columns[category="funny"]').slideDown({duration: 800, easing: 'swing'})
+    else
+        $('.columns[category="inspire"]').slideDown({duration: 800, easing: 'swing'})
 
     toggle = !toggle
 }
